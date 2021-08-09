c7bf4f5079423b317de0cebb5c4d957fb20dd313
axelyerit31 <axelyerit@gmail.com>
2021-08-09 00:33:26 -0500

    gozuu

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       title: 'Analizador Léxico',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.indigo,
+        primarySwatch: Colors.blue,
       ),
       home: HomePage(),
     );
@@ -39,22 +39,109 @@ class _HomePageState extends State<HomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+
+                  //TextField
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey[500],
+                        width: 2
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(10))
+                    ),
+                    height: 200,
+                    width: 200,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 20
+                    ),
+                    child: TextField(
+                      minLines: 10,
+                      maxLines: 20,
+                    ),
+                  ),
+
+                  //Separacion
+                  SizedBox(
+                    width: 10
+                  ),
+
+                  //Flecha
+                  Icon(
+                    Icons.arrow_forward,
+                    size: 30
+                  ),
+
+                  //Separacion
+                  SizedBox(
+                    width: 10
+                  ),
+
+                  //Area de Token
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey[500],
+                        width: 2
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(10))
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 20
+                    ),
+                    height: 200,
+                    width: 100,
+                    child: Text("Token")
+                  ),
+
+                  //Separacion
+                  SizedBox(
+                    width: 10
+                  ),
+
+                  //Area de Descripcion
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey[500],
+                        width: 2
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(10))
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 20
+                    ),
+                    height: 200,
+                    width: 200,
+                    child: Text("Descripcion")
                   )
+                ],
+              ),
+
+              SizedBox(
+                height: 20
+              ),
+              
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.red[400],
+                  padding: EdgeInsets.symmetric(
+                      horizontal: 15, vertical: 8)
                 ),
-                height: 200,
-                width: 200,
-                child: TextField(
-                  minLines: 10,
-                  maxLines: 20,
-                ),
+                onPressed: () {},
+                child: Text(
+                  "Analizar",
+                  style: TextStyle(color: Colors.white)
+                )
               )
             ]
           ),
-        )
+        ),
       )
     );
   }
